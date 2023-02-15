476088f17df970a153d25189eb8544e27e50c4fb
Sarish Beekomsing <122524977+kushul-bee@users.noreply.github.com>
2023-02-15 22:36:44 +0400

    Added Order/Order Details Table & Functionalities

--- CMS/PHP/customerorderdetails.php
+++ CMS/PHP/customerorderdetails.php
@@ -8,65 +8,24 @@ outputHeader('customerorderdetails','TabaJ-Admin');
 echo' <!--INSERT ALL CONTENT HERE-->
 <div class="product-detail">
     <p>Order details</p>
-    <h3>OrderID  <span class="orderid">#F54HGT6</span> </h3>
+    <h3>OrderID  <span class="orderid"></span> </h3>
     <table>
-      <tr>
-        <th>ProductID</th> 
-        <th>CustomerID</th>
-        <th >Product</th>
-        <th >quanity</th>
-        <th >Unit price</th>
-        <th >Total price</th>
-      </tr>
-      <tr>
-          <td class="productid">FJ32JSJ</td>
-          <td class="customerid">GJR43J3</td>
-         <td class="product">IR Obstacle Avoidance infrared Sensor Module </td>
-         <td class="quanity">2</td>
-         <td class="unit-price">Rs 50</td>
-        <td class="Total price">Rs 100</td>
-      </tr>
+      <thead>
+        <tr>
+          <th>ProductID</th> 
+          <th>CustomerID</th>
+          <th >Product</th>
+          <th >quanity</th>
+          <th >Unit price</th>
+          <th >Total price</th>
+        </tr>
+      </thead>
 
-      <tr>
-        <td class="productid">F46JUKJ</td>
-        <td class="customerid">GJR43J3</td>
-        <td class="product">IR Obstacle Avoidance infrared Sensor Module </td>
-        <td class="quanity">2</td>
-        <td class="unit-price">Rs 50</td>
-       <td class="Total price">Rs 100</td>
-     </tr>
+      <tbody id="table-body">
 
+      <tbody>
+      
 
-     <tr>
-        <td class="productid">G67JSJW</td>
-        <td class="customerid">GJR43J3</td>
-      <td class="product">IR Obstacle Avoidance infrared Sensor Module </td>
-      <td class="quanity">2</td>
-      <td class="unit-price">Rs 50</td>
-     <td class="Total price">Rs 100</td>
-   </tr>
-
-
-   <tr>
-    
-    
-    <td class="productid">FJ89JSJT</td>
-    <td class="customerid">GJR43J3</td>
-    <td class="product">Ultrasonic Ranging module HC-SR04 - 5V </td>
-    <td class="quanity">2</td>
-    <td class="unit-price">Rs 50</td>
-   <td class="Total price">Rs 100</td>
- </tr>
-
-
- <tr>
-    <td class="productid">FR45JSAJ</td>
-    <td class="customerid">GJR43J3</td>
-  <td class="product">Ultrasonic Ranging module HC-SR04 - 5V </td>
-  <td class="quanity">2</td>
-  <td class="unit-price">Rs 50</td>
- <td class="Total price">Rs 100</td>
-</tr>
 
 
  <tr>
@@ -75,11 +34,11 @@ echo' <!--INSERT ALL CONTENT HERE-->
   <td > </td>
   <td ></td>
   <td ><b>Total:</b></td>
- <td class="Total price">Rs 500</td>
+ <td id="total-price"class="Total price"></td>
 </tr>
     </table>
-    <div class="delete-order-btn"><a href="#">Delete Order</a></div>
   </div>
+  <script src="..\javascript\customerorderdetails.js"></script>
 </div>
 ';
 
--- CMS/javascript/adminviewproducts.js
+++ CMS/javascript/adminviewproducts.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function() {
     $.ajax({
         url: '../server/getProducts.php',
@@ -10,7 +8,7 @@ $(document).ready(function() {
             var tableBody = $('#table-body');
             for (var i = 0; i < products.length; i++) {
                 var product = products[i];
-                var row = '<tr>' +
+                var row = '<tr data-id="' + product._id.$oid + '">' +
                 '<td>' + product._id.$oid + '</td>' +
                 '<td>' + product.name + '</td>' +
                 '<td>' + product.category + '</td>' +
@@ -19,16 +17,43 @@ $(document).ready(function() {
                 '<td class="buttons">' +
                 '<a class="button-68" href="editproduct.php?id=' + product._id.$oid +'" id="edit" role="button">Edit</a>' +
                 '<div class="delete-button">' +
-                '<button class="button-68" id="delete" onclick="deleteCollection()" role="button">Delete</button>' +
+                '<button class="button-68 delete" id="delete" data-id="' + product._id.$oid + '">Delete</button>' +
                 '</div>' +
                 '</td>' +
                 '</tr>';
                 tableBody.append(row);
             }
+            
+            // Add a click event listener to the "Delete" buttons
+            $('.delete-button').on('click', 'button.delete', function() {
+                var productId = $(this).data('id');
+                // Confirm the deletion
+                if (confirm("Are you sure you want to delete this product?")) {
+                    deleteProduct(productId);
+                }
+            });
         },
         
         error: function(xhr, status, error) {
             alert("Error: " + error.message);
         }
     })
-})
+});
+
+// Function to delete a product from the database
+function deleteProduct(productId) {
+    $.ajax({
+        url: '../server/deleteProduct.php',
+        type: 'POST',
+        data: {
+            id: productId
+        },
+        success: function(response) {
+            // Remove the table row for the deleted product
+            $('#table-body').find('tr[data-id="' + productId + '"]').remove();
+        },
+        error: function(xhr, status, error) {
+            alert("Error: " + error.message);
+        }
+    });
+}
